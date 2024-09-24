de7590716b1414875213b3badbfb143394ca27ff
HariHaranOS <harisaravanan.2012@gmail.com>
2024-09-24 13:53:30 +0530

    Add files via upload

---
+++ Task9.java
@@ -0,0 +1,46 @@
+// 1.) write a program to add two number using static method
+// public class main{
+
+//     static int myMethod(int x,int y){
+//         return x+y;
+
+//     }
+//     public static void main (String[] args){
+//        System.out.println(myMethod(10,30));
+//     }
+// }
+
+// 2.) write a program to add two number using public method
+// public class main{
+//     public void myPublicmethod(int x,int y){
+//         System.out.println(x+y);
+//     }
+//     public static void main(String[]args){
+//         main myObj=new main();
+//         myObj.myPublicmethod(70,80);
+//     }
+// }
+
+// 3.) write a program to print a squre of the number by passing the value in the method
+// public class main{
+//     static void Square(int a,int pow){
+//         System.out.println(Math.pow(a,pow));
+//     }
+//     public static void main(String[]args){
+//         Square(2,5);
+
+//     }
+// }
+
+ /*    // 5.) write a program to reverse a integer .
+
+        public static void main(String[] args) {
+            int a=5474;
+            int rev=0;
+            while(a!=0)
+            {
+                rev=rev*10+a%10;
+                a=a/10;
+            }
+            System.out.println(rev);
+        }   */
